6f2ae5b20e58c4ba1ad5ad8e6cea13b0e25e331c
AdrianAP70 <adrianap7758@gmail.com>
2024-08-19 23:52:58 +0700

    first commit

---
+++ src/boot/axios.js
@@ -0,0 +1,15 @@
+/* eslint-disable */
+import { boot } from "quasar/wrappers";
+import axios from "axios";
+//http://localhost:4000
+const baseURL = "https://backendrumahbumn-production.up.railway.app";
+const api = axios.create({ baseURL: baseURL });
+
+export default boot(({ app, router }) => {
+  app.config.globalProperties.$axios = axios;
+
+  app.config.globalProperties.$imgUrl = baseURL;
+  app.config.globalProperties.$api = api;
+});
+
+export { api, axios };
